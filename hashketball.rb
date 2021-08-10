fe7aa0e70d4395a30370172bb5af984a88ba8836
leslieyi <leslieyi411@gmail.com>
2021-08-10 13:03:33 -0400

    Cheng Leslie

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,112 @@ def game_hash
 end
 
 # Write code here
+# Build a method, num_points_scored that takes in an argument of a player's name and returns the number of points scored for that player.
+def num_points_scored(name)
+  points_found = 0
+  game_hash[:home][:players].select {|player| 
+    if (player[:player_name] == name)
+      points_found = player[:points]
+    end 
+  }
+  game_hash[:away][:players].select {|player| 
+    if (player[:player_name] == name)
+      points_found = player[:points]
+    end 
+  }
+  points_found
+end
+
+
+# Build a method, shoe_size, that takes in an argument of a player's name and returns the shoe size for that player.
+def shoe_size(name)
+  shoe_size_total = 0
+  game_hash[:home][:players].select {|player| 
+    if (player[:player_name] == name)
+      shoe_size_total = player[:shoe]
+    end 
+  }
+  game_hash[:away][:players].select {|player| 
+    if (player[:player_name] == name)
+      shoe_size_total = player[:shoe]
+    end 
+  }
+  shoe_size_total
+
+end
+
+
+# Build a method, team_colors, that takes in an argument of the team name and returns an Array of that team's colors.
+def team_colors(name)
+  team_color = []
+  game_hash.select do |team|
+    if game_hash[team][:team_name] == name
+      team_color = game_hash[team][:colors]
+    end
+  end
+  team_color
+end
+
+# p  team_colors("Brooklyn Nets")
+# Build a method, team_names, that operates on the game Hash to return an Array of the team names.
+def team_names
+  team_array = []
+  team_array << game_hash[:home][:team_name] 
+  team_array << game_hash[:away][:team_name] 
+
+  team_array
+
+end
+
+# Build a method, player_numbers, that takes in an argument of a team name and returns an Array of the jersey numbers for that team.
+def player_numbers(name)
+  jersy_array = []
+  game_hash[:home][:players].each {|player|
+    if game_hash[:home][:team_name] == name
+      jersy_array <<  player[:number]
+    end 
+  }
+  game_hash[:away][:players].each {|player|
+    if game_hash[:away][:team_name] == name
+      jersy_array <<  player[:number]
+    end 
+  }
+  jersy_array
+end
+
+# Build a method, player_stats, that takes in an argument of a player's name and returns a hash of that player's stats.
+def player_stats(name)
+  game_hash[:home][:players].select {|players|
+    if players[:player_name] == name
+      return players
+    end 
+  }
+  game_hash[:away][:players].select {|players|
+    if players[:player_name] == name
+      return players
+    end 
+  }
+end 
+#  pp player_stats("Alan Anderson")
+
+
+# Build a method, big_shoe_rebounds, that will return the number of rebounds associated with the player that has the largest shoe size. Break this one down into steps:
+def big_shoe_rebounds
+  shoe_size = 0 
+  player_found = 0
+
+  game_hash[:home][:players].each {|player|
+    if shoe_size < player[:shoe]
+      shoe_size = player[:shoe]
+      player_found = player[:rebounds]
+    end 
+  }
+
+  game_hash[:away][:players].each {|player|
+    if shoe_size < player[:shoe]
+      shoe_size = player[:shoe]
+      player_found = player[:rebounds]
+    end 
+  }
+  player_found
+end 
